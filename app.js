2b5829c65f82d4b4e83da15c9610a32e9f8a483f
Sardor <Sardor>
2023-03-08 02:03:07 +0000

    added some codes

--- app.js
+++ app.js
@@ -12,15 +12,22 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
-
+//
+let posts = []
 app.get('/', function (req, res) {
-  res.render('home')
+  res.render('home', { posts: posts })
 })
 app.get('/addjob', function (req, res) {
   res.render('addjob')
 })
 app.post('/addjob', function (req, res) {
-  console.log(req.body.postTitle)
+  let post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  }
+
+  posts.push(post)
+  res.redirect('/')
 })
 app.get('/static', function (req, res) {
   res.render('static')
